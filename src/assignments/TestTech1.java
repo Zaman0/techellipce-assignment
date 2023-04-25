10302d51fdda3f0c3e8eaf35eac1d2121a046539
Zaman0 <sabbir0@gmail.com>
2023-04-25 00:51:35 -0400

    commit

--- src/assignments/TestTech1.java
+++ src/assignments/TestTech1.java
@@ -1,12 +1,45 @@
 package assignments;
+import java.util.Scanner;
 
 public class TestTech1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter day: ");
+		int weekdayNumber = s.nextInt(); 
 		
-		System.out.println("Hello");
+		String weekdayName;
 		
-
+		if (weekdayNumber==1) {
+			weekdayName = "Sunday";
+		}
+		else if (weekdayNumber==2) {
+			weekdayName = "Monday";
+		}
+		else if (weekdayNumber==3) {
+			weekdayName = "Tuesday";
+		}
+		else if (weekdayNumber==4) {
+			weekdayName = "Wednesday";
+		}
+		else if (weekdayNumber==5) {
+			weekdayName = "Thursday";
+		}
+		else if (weekdayNumber==6) {
+			weekdayName = "Friday";
+		}
+		
+		else if (weekdayNumber==7) {
+			weekdayName = "Saturday";
+		}
+				
+		else {
+			weekdayName = "Wrong Day";
+		}
+		
+		System.out.println("The corrosponding weekday number is: "+ weekdayName);
 	}
+	
 
 }
